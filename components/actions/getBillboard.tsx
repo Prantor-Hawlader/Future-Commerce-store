4a13de45da31de5b29daf9094a9a0ec7cdd94958
Prantor-Hawlader <prantor502@gmail.comm>
2024-04-10 19:44:26 +0600

    New database shifed

--- components/actions/getBillboard.tsx
+++ components/actions/getBillboard.tsx
@@ -2,7 +2,7 @@ import { Billboard } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 const getBillboard = async (id: string): Promise<Billboard> => {
-  const res = await fetch(`${URL}/${id}`);
+  const res = await fetch(`${URL}/5f7db8c4-ef79-430d-85d6-e5ed90ec9075`);
   return res.json();
 };
 
